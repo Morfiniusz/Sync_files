4d864e8617d8e732a54e799607da02f66b551962
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2023-01-03 17:01:24 +0100

    Added main.cpp CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main () {
+    std::cout << "Hello world ... bo od czegoś trzeba zacząć ... za kilka dni będzie to potężny program do synchronizowania plików xD xD xD\n";
+    return 0;
+}
