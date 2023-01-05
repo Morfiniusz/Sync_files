353ee7d58b32f0f57c17748b0ef9de57c8dda309
Bartlomiej <octicterbart@gmail.com>
2023-01-05 21:17:11 +0100

    Added skeleton of synchronization functions

--- main.cpp
+++ main.cpp
@@ -6,9 +6,38 @@
 #include <algorithm>
 #include <filesystem>
 
+namespace fs = std::filesystem;
 
-int main () {
-    std::cout << "Hello world ... bo od czegoś trzeba zacząć ... za kilka dni będzie to potężny program do synchronizowania plików xD xD xD\n";
+
+///
+/// \param source
+/// \param destination
+/// \param mutex
+/// \param cv
+/// \param isReady
+/// @brief The thread is woken up every few seconds and checks the folders, if changes have been made,
+/// use lock_guard to avoid race condition.
+//function to synchronize two folders
+void addDiffrenceFileToTemp(const fs::path &source,
+                            const fs::path &destination,
+                            std::mutex &mutex,
+                            std::condition_variable &cv,
+                            bool &isReady) {
+
+    std::unique_lock<std::mutex> lock(mutex); //TODO: Make this function thread safe. Add lock_guard
+
+
+    for (const auto &entry: fs::directory_iterator(source)) {
+        if (fs::is_directory(entry)) {
+        } else {
+
+        }
+    }
+}
+
+int main() {
+    std::cout
+            << "Hello world ... bo od czegoś trzeba zacząć ... za kilka dni będzie to potężny program do synchronizowania plików xD xD xD\n";
 
     return 0;
 }
