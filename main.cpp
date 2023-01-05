2d76470642041742e1f77b4126a54ff22ce9cdcd
Bartlomiej <octicterbart@gmail.com>
2023-01-05 20:59:13 +0100

    Added init include

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+#include <algorithm>
+#include <filesystem>
+
 
 int main () {
     std::cout << "Hello world ... bo od czegoś trzeba zacząć ... za kilka dni będzie to potężny program do synchronizowania plików xD xD xD\n";
+
     return 0;
 }
