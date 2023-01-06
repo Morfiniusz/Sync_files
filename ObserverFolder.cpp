54712c2699419e8b63abe994848a4f3c91752704
Bartlomiej <octicterbart@gmail.com>
2023-01-06 03:11:00 +0100

    Added ObserverFolder class

---
+++ ObserverFolder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bartl on 06.01.2023.
+//
+
+#include "ObserverFolder.h"
---
+++ ObserverFolder.h
@@ -0,0 +1,14 @@
+//
+// Created by bartl on 06.01.2023.
+//
+
+#ifndef SYNC_FILES_OBSERVERFOLDER_H
+#define SYNC_FILES_OBSERVERFOLDER_H
+
+
+class ObserverFolder {
+
+};
+
+
+#endif //SYNC_FILES_OBSERVERFOLDER_H
