c4eca1a4648f6a8590f8e9beacfddedcf814787a
Bartlomiej <octicterbart@gmail.com>
2023-01-06 03:08:39 +0100

    Added Observer interface

---
+++ Observer.h
@@ -0,0 +1,15 @@
+//
+// Created by bartl on 06.01.2023.
+//
+/// \brief Observer interface
+
+
+#ifndef SYNC_FILES_OBSERVER_H
+#define SYNC_FILES_OBSERVER_H
+
+class Observer {
+public:
+    virtual void upadate() = 0;
+};
+
+#endif //SYNC_FILES_OBSERVER_H
