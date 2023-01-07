d38b1bc06923e511cf8e599ba870ae3a895665c9
Marcin Kijewski <mkijew@poczta.fm>
2023-01-07 23:27:21 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// test 
+
 int main () {
     std::cout << "Hello world ... bo od czegoś trzeba zacząć ... za kilka dni będzie to potężny program do synchronizowania plików xD xD xD\n";
     return 0;
