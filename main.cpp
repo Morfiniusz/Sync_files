d9f6c2e4435a9db2ff7ac57bbe7304797d8723c6
Bartlomiej <octicterbart@gmail.com>
2023-01-07 16:11:34 +0100

    Added Marcin's changes

--- main.cpp
+++ main.cpp
@@ -69,9 +69,6 @@ const fs::path source = currentPath.parent_path() / "Test\\TestFolder\\MasterFol
 const fs::path destination = currentPath.parent_path() / "Test\\TestFolder\\DestinationFolder";
 
 int main() {
-    std::cout
-            << "Hello world ... bo od czegoś trzeba zacząć ... za kilka dni będzie to potężny program do synchronizowania plików xD xD xD\n";
-
     std::cout << "Source: " << source << std::endl;
     std::cout << "Destination: " << destination << std::endl;
 
