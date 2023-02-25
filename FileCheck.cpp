332a9b794ab7770b7cd572473c576ff764f1a5d9
Bartlomiej <octicterbart@gmail.com>
2023-02-25 15:03:07 +0100

    Added md5

---
+++ FileCheck.cpp
@@ -0,0 +1,33 @@
+//
+// Created by bartl on 25.02.2023.
+//
+
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include "FileCheck.h"
+
+namespace fs = std::filesystem;
+
+FileCheck::FileCheck(const fs::path &folderPath) {
+    // Get the MD5 file path
+    fs::path md5file = folderPath / "MD5_CHECKSUM";	
+
+    // Check if the checksum file exists in the folder
+    if (fs::exists(md5file)) {
+        //Open the MD5 file
+        std::ifstream md5fstream(md5file, std::ios::binary);
+        std::string buffer;
+        while (std::getline(md5fstream, buffer)) {
+            //Split the string by ',' and store the first part into fileName
+            std::string fileName = buffer.substr(0, buffer.find(','));
+            //Store the second part into hashValue
+            std::string hashValue = buffer.substr(buffer.find(',') + 1);
+
+            // Add the file name and hash value to the map
+            m_map[fileName] = hashValue;
+        }
+    }
+}
+
+
---
+++ FileCheck.h
@@ -0,0 +1,19 @@
+//
+// Created by bartl on 25.02.2023.
+//
+
+#ifndef SYNC_FILES_FILECHECK_H
+#define SYNC_FILES_FILECHECK_H
+
+using fs = std::filesystem
+
+
+class FileCheck {
+public:
+    FileCheck(const std::filesystem::path &folderPath);
+    //add md5 file checking
+
+};
+
+
+#endif //SYNC_FILES_FILECHECK_H
